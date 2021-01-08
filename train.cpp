b514704ccd30c87747e96dcab73b267e50f3939a
frozen293 <65149862+frozen293@users.noreply.github.com>
2021-01-08 21:30:28 +0530

    Add files via upload

---
+++ train.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		if(n%8==0)
+		{
+			cout<<n-1<<"SL"<<endl;
+		}
+		else if(n%8==7)
+		{
+			cout<<n+1<<"SU"<<endl;
+		}
+		else if(n%8==6)
+		{
+			cout<<n-3<<"UB"<<endl;
+		}
+		else if(n%8==3)
+		{
+			cout<<n+3<<"UB"<<endl;
+		}
+		else if(n%8==5)
+		{
+			cout<<n-3<<"MB"<<endl;
+		}
+		else if(n%8==2)
+		{
+			cout<<n+3<<"MB"<<endl;
+		}
+			else if(n%8==4)
+		{
+			cout<<n-3<<"LB"<<endl;
+		}
+			else if(n%8==1)
+		{
+			cout<<n+3<<"LB"<<endl;
+		}
+		
+	}
+	return 0;
+}
